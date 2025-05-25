b54d81daba35c8434f89b2d22c193b893fb94ef7
Ali-Rahimpoor <ali.rahimpoor.ar@gmail.com>
2025-05-25 18:45:32 +0330

    feat: addtextChange & counter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useState,useRef,useEffect } from "react";
 import { useAddCommentMutation } from "../features/apiSlice";
 
 const Header = ()=>{
     const [badgeLetter,setBadgeLetter] = useState('');
     const [company,setCompany] = useState('');
     const [text,setText] = useState('');
-    
     const [addComment] = useAddCommentMutation();
+
+    const remainCharRef = useRef(150);
+    const counterElement = useRef(null);
+
+
+    const handleTextChange = (e) => {
+    const inputText = e.target.value;
+    setText(inputText);
+    remainCharRef.current = 150 - inputText.length;
+    
+    // به روزرسانی DOM مستقیماً بدون رندر مجدد
+    if (counterElement.current) {
+      counterElement.current.textContent = remainCharRef.current;
+      
+      // تغییر رنگ اگر کاراکترهای باقیمانده کم باشد
+      if (remainCharRef.current < 20) {
+        counterElement.current.classList.add('text-rose-400');
+      } else {
+        counterElement.current.classList.remove('text-rose-400');
+      }
+    }
+  };
+
 
 const handleClickSubmit = async () => {
   if (text.includes("#") && text.length > 5) {
@@ -25,6 +47,7 @@ const handleClickSubmit = async () => {
       setCompany('');
       setBadgeLetter('');
       setText('')
+      counterElement.current.textContent = 150;
     } catch (err) {
       console.error("Submit failed:", err);
     }
@@ -42,11 +65,11 @@ const handleClickSubmit = async () => {
     <h1 className="sm:text-4xl  text-2xl font-Funnel">Give Feedback. <span className="font-Montserrat-Light text-base sm:text-xl">Publicly.</span></h1>
    
     <div className="shadow border border-gray-800 w-[299px] sm:w-[400px] h-[150px] sm:h-[140px] rounded-md bg-zinc-700 mt-9 flex flex-col items-center justify-between p-2">
-        <textarea value={text} onChange={(e)=>setText(e.target.value)} id="textarea" className="text-zinc-200 text-sm font-Inter focus:outline-none resize-none bg-zinc-700 w-full  flex-grow placeholder:text-zinc-400 sm:placeholder:text-base" maxLength="150"  placeholder="Enter Your feedback hear, remember to #hashtag the company!"></textarea>
+        <textarea value={text} onChange={handleTextChange} id="textarea" className="text-zinc-200 text-sm font-Inter focus:outline-none resize-none bg-zinc-700 w-full  flex-grow placeholder:text-zinc-400 sm:placeholder:text-base" maxLength="150"  placeholder="Enter Your feedback hear, remember to #hashtag the company!"></textarea>
         
         <div className="flex justify-between w-full px-2 items-center">
-            <p id="counter" className="sm:text-sm text-xs font-Inter-Light ">150</p>
-            <button id="submit-btn" onClick={()=>handleClickSubmit()} className="bg-white text-zinc-800 p-2 rounded-3xl font-Inter sm:text-base text-sm transition-all hover:scale-105">Submit</button>
+            <p id="counter" ref={counterElement} className="sm:text-sm text-xs font-Inter-Light ">150</p>
+            <button id="submit-btn" onClick={()=>handleClickSubmit()} className="bg-white cursor-pointer hover:bg-zinc-300 text-zinc-800 p-2 rounded-3xl font-Inter sm:text-base text-sm transition-all hover:scale-105">Submit</button>
         </div>
     </div>
    </div>
